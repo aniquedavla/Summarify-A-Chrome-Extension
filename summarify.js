4cf4d02e90740d9a5def828b52261991a221330d
Anique <aniquedavla@Aniques-MacBook-Pro.local>
2016-04-04 23:15:59 -0700

    working with html -any url input

--- summarify.js
+++ summarify.js
@@ -3,22 +3,21 @@ $(function() {
 	//var $addSummary = $('#summary');
 	// var $senten = $('#numberofSentences');
 	var $urlT = $('#feed_url');
-	$("#data").submit(function(event) {
-		console.log($urlT.val())
+	$('#Data').submit(function(event) {
+		console.log($urlT.val());
 		$.ajax({
 			url: "https://api-2445581399224.apicast.io/api/v1/summarize?",
 			type:"GET",
-			data: {"sourceURL":"http://www.bbc.com/news/world-asia-35940067","user_key":"32c37493e717e97d75b64d2a8145bdbb"},
+			data: {"sourceURL":$urlT.val(),"user_key":"32c37493e717e97d75b64d2a8145bdbb"},
 			success: function(data){
 				$.each(data, function(i, val) {
-					$('#summary').append(val);
+					$('#summary').append(val.summary);
 				});
 
 			},
 			error: function(jqXHR, textStatus, errorThrown){
 				console.log(errorThrown);
 			}
-			event.preventDefault();
 		});
 
 
@@ -34,6 +33,8 @@ $(function() {
 	// 			alert("NOT Summarizing, check the url or the internet!")
 	// 		}
 	// 	}); 
+	event.preventDefault();
+
 
 });
 
