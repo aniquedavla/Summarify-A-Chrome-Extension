d6de72659c8c939c2b8d449f3f7222ce99562f3b
Anique <aniquedavla@Aniques-MacBook-Pro.local>
2016-04-08 15:10:57 -0700

    smmary appends

--- summarify.js
+++ summarify.js
@@ -3,21 +3,31 @@ $(function() {
 	//var $addSummary = $('#summary');
 	// var $senten = $('#numberofSentences');
 	var $urlT = $('#feed_url');
-	$('#Data').submit(function(event) {
-		console.log($urlT.val());
-		$.ajax({
-			url: "https://api-2445581399224.apicast.io/api/v1/summarize?",
-			type:"GET",
-			data: {"sourceURL":$urlT.val(),"user_key":"32c37493e717e97d75b64d2a8145bdbb"},
+	// var $loading = $('#summary');
+//form is submitted then it make the api call
+$('#Data').submit(function(event) {
+	console.log($urlT.val());
+// ajax makes a GET call to sentisum passing in the url and api key as parameters
+$.ajax({
+	url: "https://api-2445581399224.apicast.io/api/v1/summarize?",
+	type:"GET",
+	data: {"sourceURL":$urlT.val(),"user_key":"32c37493e717e97d75b64d2a8145bdbb", "summaryPercent":"50"},
+			//all summary appened to a div from json element
 			success: function(data){
-				$.each(data, function(i, val) {
-					$('#summary').append(val.summary);
-				});
+				console.log(data);
+				$('#summary').append(data.summary);
+				
+
+				// $.each(data, function(i, val) {
+				// 	console.log(val);
+				// 	$('#summary').append(val);
+				// });
 
 			},
 			error: function(jqXHR, textStatus, errorThrown){
 				console.log(errorThrown);
 			}
+			
 		});
 
 
@@ -33,6 +43,7 @@ $(function() {
 	// 			alert("NOT Summarizing, check the url or the internet!")
 	// 		}
 	// 	}); 
+	
 	event.preventDefault();
 
 
